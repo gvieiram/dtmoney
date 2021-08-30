30fe621b4dd3a1e608510b219214be207ad51c72
Gustavo Vieira Martins <gustavoafcavai@gmail.com>
2021-08-30 11:34:30 -0300

    :sparkles: Aplicação finalizada - Adicionado hook

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Modal from 'react-modal';
 import { GlobalStyle } from "./styles/global";
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
-import { TransactionsProvider } from "./TransactionsContext";
+import { TransactionsProvider } from "./hooks/useTransactions";
 import { NewTransactionModal } from "./components/NewTransactionModal";
 
 Modal.setAppElement('#root');
